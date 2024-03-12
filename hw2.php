6321a9be52d03755c2be18f4b6e5887576c3daaa
Jame34 <150788923+Jame34@users.noreply.github.com>
2024-03-12 10:53:21 +0800

    Add files via upload

---
+++ hw2.php
@@ -0,0 +1,53 @@
+<html>
+
+<head>
+
+</head>
+<title>
+HW2  履歷
+</title>
+
+<body bgcolor=#d5ebe9>
+
+<font size=15 color=#9ab6ed><center><h1><b>薛天乙</b></h1></center></font>
+
+<hr color="#0a0a0a"></hr>
+
+<center><img src="img\d4380597-fbf3-4fd4-bb71-80ca0d7c0622.jpg"></center>
+<hr color="#0a0a0a"></hr>
+<font size=15 color="#9ab6ed"><center><h2><b>個人簡介</b></h2></center></font>
+<font size=7 color="#046466"><b><center><ol></center>
+1. 學校：國立高雄大學<br>
+2. 學系：資訊管理學系<br>
+3. 僑居地：馬來西亞<br>
+4. 專長：耍廢，程式是會不了一點<br>
+5. 夢想：躺在家就可以賺錢錢<br>
+</ol>
+<hr color="#0a0a0a"></hr>
+<font size=15 color="#9ab6ed"><center><h3><b>自我介紹</b></h3></center></font>
+<p>
+我是來自馬來西亞的僑生，目前雖然大四，但還有好多課還沒修，一半是好課值得一修再修，一半是因為之前大二有轉系的念頭，但最終還是放棄了o(╥﹏╥)o<br>
+曾經是僑聯會會長，學習到了很多書上沒辦法學到的知識，比如如何有效的處理問題，人際關係等等。<br>
+目前對未來沒有太多的想法，只希望可以盡早把課都修完回家當米蟲，因為真的好想家┭┮﹏┭┮<br>
+個人還是比較喜歡運動，是系羽裡的老將，還是希望可以為系羽拿到更多的榮譽(*^▽^*)<br>
+其實在課業上都比較壓力，因為不擅長程式，不過其他論理類的還可以(p≧w≦q)
+</p>
+
+</b>
+<hr color="#0a0a0a"></hr>
+<center><table width="1000"  height="500" border=1>
+    <tr><th><font size=10 color="#9ab6ed">順序</font></th><th><font size=10 color="#9ab6ed">球類</font></th><th><font size=10 color="#9ab6ed">品牌</font></th></tr>
+    <tr><td><font size=10 color="#9ab6ed"><center>1</center></font></td>
+    <td><font size=10 color="#9ab6ed"><center>羽球</center></font></td>
+    <td><font size=10 color="#9ab6ed"><center>勝利</center></font></td></tr>
+    <tr><td><font size=10 color="#9ab6ed"><center>2</center></font></td>
+    <td><font size=10 color="#9ab6ed"><center>籃球</center></font></td>
+    <td><font size=10 color="#9ab6ed"><center>摩騰</center></font></td></tr>
+    <tr><td><font size=10 color="#9ab6ed"><center>3</center></font></td>
+    <td><font size=10 color="#9ab6ed"><center>排球</center></font></td>
+    <td><font size=10 color="#9ab6ed"><center>米卡薩</center></font></td></tr>
+</table></center>
+
+</body>
+
+</html>
